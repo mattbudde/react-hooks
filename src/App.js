f5ca132ac37a76a7846fb572b263253c607dce75
mattbudde <matt@designbymatt.org>
2020-01-03 15:47:27 +0000

    Init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useRef, createContext } from 'react';
+import Toggle from './Toggle';
+import { useTitleInput } from './hooks/useTitleInput';
+
+const UserContext = createContext();
 
 const App = () => {
+  const [name, setName] = useTitleInput('');
+  const ref = useRef();
+
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
-    </div>
+    <UserContext.Provider>
+      <div className="main-wrapper" ref={ref}>
+        <h1 onClick={() => console.log(ref.current.className)}>
+          Level Up Dishes
+        </h1>
+        <Toggle />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
